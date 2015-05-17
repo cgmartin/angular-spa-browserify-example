55a3a4fab690c8d25859405c76bad9d13b7be9bd
Christopher Martin <cgmartin@gmail.com>
2015-05-17 15:45:11 -0400

    Fix removed partial

--- src/error/partials/index.js
+++ src/error/partials/index.js
@@ -2,6 +2,5 @@
 
 module.exports = {
     error:    require('./error.partial.html'),
-    errorException: require('./error-exception.partial.html'),
     error404: require('./error-404.partial.html')
 };
