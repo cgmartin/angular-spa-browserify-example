6148af70586788884bf51458aaace45c7c6558b6
Christopher Martin <cgmartin@gmail.com>
2015-05-24 23:21:14 -0400

    Add throng for concurrency

--- server/static-server.js
+++ server/static-server.js
@@ -1,7 +1,16 @@
 'use strict';
 // Starts a static server for the client app
+var throng = require('throng');
 var staticServer = require('spa-express-static-server');
-staticServer.start({
-    webRootPath: process.env.STATIC_WEBROOT || './dist',
-    spaBoot:     require('./spa-boot')
+
+throng(start, {
+    workers: process.env.WEB_CONCURRENCY || 1,
+    lifetime: Infinity
 });
+
+function start() {
+    staticServer.start({
+        webRootPath: process.env.STATIC_WEBROOT || './dist',
+        spaBoot:     require('./spa-boot')
+    });
+}
